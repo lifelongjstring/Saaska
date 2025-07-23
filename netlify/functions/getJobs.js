011f72618e41151a09f7d4e5dc57e1f9c6682937
Justin Blair <ncd016@usask.ca>
2025-07-23 11:43:14 -0600

    Moved netlify up one level in directory to allow netlify to parse

---
+++ netlify/functions/getJobs.js
@@ -0,0 +1,12 @@
+export async function handler() {
+    const appID = process.env.ADZUNA_APP_ID;
+    const appKey = process.env.ADZUNA_APP_KEY;
+
+    const res = await fetch(`https://api.adzuna.com/v1/api/jobs/gb/search/1?app_id=${appID}&app_key=${appKey}&results_per_page=20&what=javascript&where=Saskatoon`);
+    const data = await res.json();
+
+    return {
+        statusCode: 200,
+        body: JSON.stringify(data),
+    }
+}
