1465f43c9f3f36769e4c23b9a49c87c10c46d53a
Justin Blair <ncd016@usask.ca>
2025-07-23 11:35:03 -0600

    Update getJobs

--- public/netlify/functions/getJobs.js
+++ public/netlify/functions/getJobs.js
@@ -2,7 +2,7 @@ export async function handler() {
     const appID = process.env.ADZUNA_APP_ID;
     const appKey = process.env.ADZUNA_APP_KEY;
 
-    const res = await fetch(`https://api.adzuna.com/v1/api/jobs/gb/search/1?app_id=${appID}&app_key=${appKey}&results_per_page=20&what=javascript&where=london`);
+    const res = await fetch(`https://api.adzuna.com/v1/api/jobs/gb/search/1?app_id=${appID}&app_key=${appKey}&results_per_page=20&what=javascript&where=Saskatoon`);
     const data = await res.json();
 
     return {
